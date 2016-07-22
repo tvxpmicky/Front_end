ad65c7aad8128820f38118186452557f042a7f68
tvxpmicky <tvxpmicky@nate.com>
2016-07-22 17:48:11 +0900

    no message

---
+++ class/webpage/js/gnb.js
@@ -0,0 +1,11 @@
+/**
+ * Created by 321-10 on 2016-07-22.
+ */
+
+$(function(){
+
+    $('.gnb-depth1-link').on('mouseenter',function(){
+        $('.gnb-depth2').show();
+        $('.gnb-bg').show();
+    });
+});
